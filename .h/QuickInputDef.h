d708cd609b16edd3ab156b4b24972e518bf36f17
ChiyukiGana <101380693+ChiyukiGana@users.noreply.github.com>
2024-01-03 22:59:43 +0800

    Move: uint32 to int32

--- .h/QuickInputDef.h
+++ .h/QuickInputDef.h
@@ -80,7 +80,7 @@ struct Action
 
 	struct Delay { uint32 ms; uint32 ex; };
 	struct Key { enum { up, down, click }; uint32 vk = 0; uint32 state = up; };
-	struct Mouse { uint32 x = 0; uint32 y = 0; uint32 ex = 0; bool move = 0; };
+	struct Mouse { int32 x = 0; int32 y = 0; uint32 ex = 0; bool move = 0; };
 	struct Text { String::wcstring str; };
 	struct Color { Rgba rgbe = 0; RECT rect = { 0 }; bool unfind = 0; bool move = 0; Actions next; };
 	struct Loop { uint32 count; Actions next; };
